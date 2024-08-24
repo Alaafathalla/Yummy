39fb84347c62d5854e943f6d27d798012a2c37eb
Alaafathalla <alaafathalla36@gmail.com>
2024-08-24 08:01:53 +0300

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from '../src/components/Home/Home.jsx';
 import Card from '../src/components/Card/Card.jsx';
-import Layout from '../src/components/Layout/Layout.jsx';  // Assuming you have a Layout component
-import About from '../src/components/About/About.jsx';  // Assuming you have an About component
+import Layout from '../src/components/Layout/Layout.jsx';  
+import About from '../src/components/About/About.jsx';  
 
 import './App.css';
 
 let routers = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />,  // Use the Layout component directly
+    element: <Layout />,  
     children: [
-      { index: true, element: <Home /> },  // Use the Home component directly
+      { index: true, element: <Home /> },  
       { path: "home", element: <Home /> },
-      { path: "about", element: <About /> },  // Make sure you have an About component
+      { path: "about", element: <About /> },  
       { path: "card", element: <Card /> },
     ],
   },
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import styles from './Home.module.css';
+import axios from "axios";
 
-const Home = () => (
-  <div className={styles.Home}>
-    Home Component
-  </div>
-);
-
-Home.propTypes = {};
-
-Home.defaultProps = {};
-
+const (meals,setMeals)=useState([])
 export default Home;
+<>
+
+async function getData(meal) {
+ let data= await.axios.get(www.themealdb.com/api/json/v1/1/search.php?s=);
+ 
+ setMeal (data.meals)
+
+
+useEffect(()=>getData();}
+,[])
+
+</>
