a9c2080953cb0e0371429e6d89afaa230613fcb4
Alaafathalla <alaafathalla36@gmail.com>
2024-09-12 05:58:15 +0300

    details

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,33 +1,56 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
-export default function 
-() {
+export default function MealDetails() {
+  let { id } = useParams();
+  const [details, setDetails] = useState({});
+
+  useEffect(() => {
+    async function getDetails() {
+      try {
+        const { data } = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+        setDetails(data.meals[0]);
+      } catch (error) {
+        console.error("Error fetching meal details:", error);
+      }
+    }
+
+    getDetails();
+  }, [id]); 
+
   return (
-        <>
-
-      <div className="row">
-        <div className="col-md-6 p-5">
-          <div className="">
-            <img className='w-100' src={mealDetails.strMealThumb} alt="" />
-          </div>
-        </div>
-        <div className="col-md-6 p-5">
-        <span className="badge text-bg-secondary fs-4">{mealDetails.strMeal}</span>
-          <p><h3> Instructions:</h3> {mealDetails.strInstructions && mealDetails.strInstructions.split(' ').slice(3, 40).join(' ')}</p>
-          <h3>Area : {mealDetails.strArea}</h3>
-          <h3>Category : {mealDetails.strCategory}</h3>
-          <h3> Details :  {mealDetails.ingredients?.map( (item, index)=>
-          <div className="d-flex flex-row">
-                <span className="badge text-bg-warning m-1 fs-6" key={index}> {item} </span>
-          </div>
-          )}</h3>
-          <a className="btn btn-primary fs-4 m-1"  href = { `${mealDetails.strSource}`} target='_blank'> Source  </a>
-          <a className="btn btn-danger fs-4 m-1"  href = { `${mealDetails.strYoutube}`} target='_blank' > Youtube  </a>
-
+    <div className="row mx-2">
+      <div className="col-md-6 p-5">
+        <div className='p-5'>
+          <img className="w-100 rounded-3" src={details.strMealThumb} alt={details.strMeal} />
         </div>
       </div>
-    </> 
-
-  )
+      <div className="col-md-6 p-5">
+        <span className="badge text-bg-secondary fs-4">{details.strMeal}</span>
+        <p>
+          <h3>Instructions:</h3> {details.strInstructions}
+        </p>
+        <h3>Area: {details.strArea}</h3>
+        <h3>Category: {details.strCategory}</h3>
+        <h3>Ingredients:</h3>
+        <div className="d-flex flex-row flex-wrap">
+          {Object.keys(details)
+            .filter(key => key.includes('strIngredient') && details[key])
+            .map((item, index) => (
+              <span className="badge text-bg-warning m-1 fs-6" key={index}>{details[item]}</span>
+            ))}
+        </div>
+        {details.strSource && (
+          <a className="btn btn-primary fs-4 m-1" href={details.strSource} target="_blank" rel="noreferrer">Source</a>
+        )}
+        {details.strYoutube && (
+          <a className="btn btn-danger fs-4 m-1" href={details.strYoutube} target="_blank" rel="noreferrer">YouTube</a>
+        )}
+      </div>
+    </div>
+  );
 }
+
+
 
